13713a88009f4d9192913a7d8ef587cc11534b64
Sebastian Cohnen <tisba@users.noreply.github.com>
2022-02-24 09:26:55 +0100

    copies and adjusts FaradayMiddleware::FollowRedirects

--- lib/faraday/follow_redirects.rb
+++ lib/faraday/follow_redirects.rb
@@ -1,28 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'follow_redirects/middleware'
+require_relative 'follow_redirects/redirect_limit_reached'
 require_relative 'follow_redirects/version'
 
 module Faraday
-  # This will be your middleware main module, though the actual middleware implementation will go
-  # into Faraday::FollowRedirects::Middleware for the correct namespacing.
   module FollowRedirects
-    # Faraday allows you to register your middleware for easier configuration.
-    # This step is totally optional, but it basically allows users to use a
-    # custom symbol (in this case, `:follow_redirects`), to use your middleware in their connections.
-    # After calling this line, the following are both valid ways to set the middleware in a connection:
-    # * conn.use Faraday::FollowRedirects::Middleware
-    # * conn.use :follow_redirects
-    # Without this line, only the former method is valid.
-    Faraday::Middleware.register_middleware(follow_redirects: Faraday::FollowRedirects::Middleware)
-
-    # Alternatively, you can register your middleware under Faraday::Request or Faraday::Response.
-    # This will allow to load your middleware using the `request` or `response` methods respectively.
-    #
-    # Load middleware with conn.request :follow_redirects
-    # Faraday::Request.register_middleware(follow_redirects: Faraday::FollowRedirects::Middleware)
-    #
-    # Load middleware with conn.response :follow_redirects
-    # Faraday::Response.register_middleware(follow_redirects: Faraday::FollowRedirects::Middleware)
+    Faraday::Response.register_middleware(follow_redirects: Faraday::FollowRedirects::Middleware)
   end
 end
--- lib/faraday/follow_redirects/middleware.rb
+++ lib/faraday/follow_redirects/middleware.rb
@@ -2,39 +2,154 @@
 
 module Faraday
   module FollowRedirects
-    # This class provides the main implementation for your middleware.
-    # Your middleware can implement any of the following methods:
-    # * on_request - called when the request is being prepared
-    # * on_complete - called when the response is being processed
+    # Public: Follow HTTP 301, 302, 303, 307, and 308 redirects.
     #
-    # Optionally, you can also override the following methods from Faraday::Middleware
-    # * initialize(app, options = {}) - the initializer method
-    # * call(env) - the main middleware invocation method.
-    #   This already calls on_request and on_complete, so you normally don't need to override it.
-    #   You may need to in case you need to "wrap" the request or need more control
-    #   (see "retry" middleware: https://github.com/lostisland/faraday/blob/main/lib/faraday/request/retry.rb#L142).
-    #   IMPORTANT: Remember to call `@app.call(env)` or `super` to not interrupt the middleware chain!
+    # For HTTP 301, 302, and 303, the original GET, POST, PUT, DELETE, or PATCH
+    # request gets converted into a GET. With `:standards_compliant => true`,
+    # however, the HTTP method after 301/302 remains unchanged. This allows you
+    # to opt into HTTP/1.1 compliance and act unlike the major web browsers.
+    #
+    # This middleware currently only works with synchronous requests; i.e. it
+    # doesn't support parallelism.
+    #
+    # If you wish to persist cookies across redirects, you could use
+    # the faraday-cookie_jar gem:
+    #
+    #   Faraday.new(:url => url) do |faraday|
+    #     faraday.use FaradayMiddleware::FollowRedirects
+    #     faraday.use :cookie_jar
+    #     faraday.adapter Faraday.default_adapter
+    #   end
     class Middleware < Faraday::Middleware
-      # This method will be called when the request is being prepared.
-      # You can alter it as you like, accessing things like request_body, request_headers, and more.
-      # Refer to Faraday::Env for a list of accessible fields:
-      # https://github.com/lostisland/faraday/blob/main/lib/faraday/options/env.rb
+      # HTTP methods for which 30x redirects can be followed
+      ALLOWED_METHODS = Set.new %i[head options get post put patch delete]
+      # HTTP redirect status codes that this middleware implements
+      REDIRECT_CODES  = Set.new [301, 302, 303, 307, 308]
+      # Keys in env hash which will get cleared between requests
+      ENV_TO_CLEAR    = Set.new %i[status response response_headers]
+
+      # Default value for max redirects followed
+      FOLLOW_LIMIT = 3
+
+      # Regex that matches characters that need to be escaped in URLs, sans
+      # the "%" character which we assume already represents an escaped sequence.
+      URI_UNSAFE = %r{[^\-_.!~*'()a-zA-Z\d;/?:@&=+$,\[\]%]}.freeze
+
+      AUTH_HEADER = 'Authorization'
+
+      # Public: Initialize the middleware.
       #
-      # @param env [Faraday::Env] the environment of the request being processed
-      def on_request(env)
-        # Do something with the request environment...
-        # This method is optional.
+      # options - An options Hash (default: {}):
+      #     :limit                      - A Numeric redirect limit (default: 3)
+      #     :standards_compliant        - A Boolean indicating whether to respect
+      #                                  the HTTP spec when following 301/302
+      #                                  (default: false)
+      #     :callback                   - A callable used on redirects
+      #                                  with the old and new envs
+      #     :cookies                    - An Array of Strings (e.g.
+      #                                  ['cookie1', 'cookie2']) to choose
+      #                                  cookies to be kept, or :all to keep
+      #                                  all cookies (default: []).
+      #     :clear_authorization_header - A Boolean indicating whether the request
+      #                                  Authorization header should be cleared on
+      #                                  redirects (default: true)
+      def initialize(app, options = {})
+        super(app)
+        @options = options
+
+        @convert_to_get = Set.new [303]
+        @convert_to_get << 301 << 302 unless standards_compliant?
       end
 
-      # This method will be called when the response is being processed.
-      # You can alter it as you like, accessing things like response_body, response_headers, and more.
-      # Refer to Faraday::Env for a list of accessible fields:
-      # https://github.com/lostisland/faraday/blob/main/lib/faraday/options/env.rb
-      #
-      # @param env [Faraday::Env] the environment of the response being processed.
-      def on_complete(env)
-        # Do something with the response environment...
-        # This method is optional.
+      def call(env)
+        perform_with_redirection(env, follow_limit)
+      end
+
+      private
+
+      def convert_to_get?(response)
+        !%i[head options].include?(response.env[:method]) &&
+          @convert_to_get.include?(response.status)
+      end
+
+      def perform_with_redirection(env, follows)
+        request_body = env[:body]
+        response = @app.call(env)
+
+        response.on_complete do |response_env|
+          if follow_redirect?(response_env, response)
+            raise RedirectLimitReached, response if follows.zero?
+
+            new_request_env = update_env(response_env.dup, request_body, response)
+            callback&.call(response_env, new_request_env)
+            response = perform_with_redirection(new_request_env, follows - 1)
+          end
+        end
+        response
+      end
+
+      def update_env(env, request_body, response)
+        redirect_from_url = env[:url].to_s
+        redirect_to_url = safe_escape(response['location'] || '')
+        env[:url] += redirect_to_url
+
+        ENV_TO_CLEAR.each { |key| env.delete key }
+
+        if convert_to_get?(response)
+          env[:method] = :get
+          env[:body] = nil
+        else
+          env[:body] = request_body
+        end
+
+        clear_authorization_header(env, redirect_from_url, redirect_to_url)
+
+        env
+      end
+
+      def follow_redirect?(env, response)
+        ALLOWED_METHODS.include?(env[:method]) &&
+          REDIRECT_CODES.include?(response.status)
+      end
+
+      def follow_limit
+        @options.fetch(:limit, FOLLOW_LIMIT)
+      end
+
+      def standards_compliant?
+        @options.fetch(:standards_compliant, false)
+      end
+
+      def callback
+        @options[:callback]
+      end
+
+      # Internal: escapes unsafe characters from an URL which might be a path
+      # component only or a fully qualified URI so that it can be joined onto an
+      # URI:HTTP using the `+` operator. Doesn't escape "%" characters so to not
+      # risk double-escaping.
+      def safe_escape(uri)
+        uri = uri.split('#')[0] # we want to remove the fragment if present
+        uri.to_s.gsub(URI_UNSAFE) do |match|
+          "%#{match.unpack('H2' * match.bytesize).join('%').upcase}"
+        end
+      end
+
+      def clear_authorization_header(env, from_url, to_url)
+        return env if redirect_to_same_host?(from_url, to_url)
+        return env unless @options.fetch(:clear_authorization_header, true)
+
+        env[:request_headers].delete(AUTH_HEADER)
+      end
+
+      def redirect_to_same_host?(from_url, to_url)
+        return true if to_url.start_with?('/')
+
+        from_uri = URI.parse(from_url)
+        to_uri = URI.parse(to_url)
+
+        [from_uri.scheme, from_uri.host, from_uri.port] ==
+          [to_uri.scheme, to_uri.host, to_uri.port]
       end
     end
   end
