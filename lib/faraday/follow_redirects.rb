086d6383282a36c74e6e0715d4da9bad3598eece
Sebastian Cohnen <tisba@users.noreply.github.com>
2022-02-24 09:45:09 +0100

    adds exceptions to rubocop config for now

--- lib/faraday/follow_redirects.rb
+++ lib/faraday/follow_redirects.rb
@@ -5,6 +5,7 @@ require_relative 'follow_redirects/redirect_limit_reached'
 require_relative 'follow_redirects/version'
 
 module Faraday
+  # Main Faraday::FollowRedirects module.
   module FollowRedirects
     Faraday::Response.register_middleware(follow_redirects: Faraday::FollowRedirects::Middleware)
   end
